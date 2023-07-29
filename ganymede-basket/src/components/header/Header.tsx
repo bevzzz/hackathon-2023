812fa4a263ff103d814306a97e9fd65a31fc1f31
seyyid <seyyid.tiryaki@gmail.com>
2023-07-29 22:19:31 +0200

    feat: header

---
+++ ganymede-basket/src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header(): JSX.Element {
+    return (
+        <div className='fixed top-0 bg-[#03C9A9]/75 w-full h-14 text-white flex backdrop-blur-md'>
+            <div className='m-auto text-2xl'>
+                Ganymede
+            </div>
+        </div>
+    )
+}
--- ganymede-basket/src/pages/list.tsx
+++ ganymede-basket/src/pages/list.tsx
@@ -7,11 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <div className='fixed top-0 bg-[#03C9A9]/75 w-full h-14 text-white flex backdrop-blur-md'>
-        <div className='m-auto text-2xl'>
-          Ganymede
-        </div>
-      </div>
       <div className="mt-20 mb-20 container px-4">
         <Shoplist></Shoplist>
       </div>
