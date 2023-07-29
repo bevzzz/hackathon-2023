05331cb7a6da0ae15cf2cf99f87be30c833fff82
seyyid <seyyid.tiryaki@gmail.com>
2023-07-29 21:07:54 +0200

    feat: reset text

--- ganymede-basket/src/components/autocompleter/Autocompleter.tsx
+++ ganymede-basket/src/components/autocompleter/Autocompleter.tsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 export default function Autocompleter(props: { addItem: Function }): JSX.Element {
     const [text, setText] = useState('');
+    const handleAdd = () => {
+        props.addItem(text);
+        setText("");
+    }
     return (
         <div className='mt-2 mb-4 flex bg-slate-200 rounded-xl'>
-            <input value={text} onKeyDown={(event: any) => { if (event.key === "Enter") props.addItem(text) }} onChange={e => setText(e.target.value)} type="text" placeholder='Name of Product...' className='autocomplete-input'></input>
-            <button onClick={() => props.addItem(text)} className='m-auto pr-4 pl-2'>+</button>
+            <input value={text} onKeyDown={(event: any) => { if (event.key === "Enter") handleAdd() }} onChange={e => setText(e.target.value)} type="text" placeholder='Name of Product...' className='autocomplete-input'></input>
+            <button onClick={() => handleAdd()} className='m-auto pr-4 pl-2'>+</button>
         </div>
     )
 }
