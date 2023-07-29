c3fa3666d23922d3074e89fe32dcc475fd5ecad7
seyyid <seyyid.tiryaki@gmail.com>
2023-07-29 21:10:32 +0200

    chore: structure

--- ganymede-basket/src/components/autocompleter/Autocompleter.tsx
+++ ganymede-basket/src/components/autocompleter/Autocompleter.tsx
@@ -8,8 +8,21 @@ export default function Autocompleter(props: { addItem: Function }): JSX.Element
     }
     return (
         <div className='mt-2 mb-4 flex bg-slate-200 rounded-xl'>
-            <input value={text} onKeyDown={(event: any) => { if (event.key === "Enter") handleAdd() }} onChange={e => setText(e.target.value)} type="text" placeholder='Name of Product...' className='autocomplete-input'></input>
-            <button onClick={() => handleAdd()} className='m-auto pr-4 pl-2'>+</button>
+            <input
+                value={text}
+                onKeyDown={(event: any) => {
+                    if (event.key === "Enter") handleAdd()
+                }}
+                onChange={e => setText(e.target.value)}
+                type="text"
+                placeholder='Name of Product...'
+                className='autocomplete-input' />
+            <button
+                onClick={() => handleAdd()}
+                className='m-auto pr-4 pl-2'
+            >
+                +
+            </button>
         </div>
     )
 }
