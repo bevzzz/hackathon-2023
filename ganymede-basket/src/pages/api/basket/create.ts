140607c1665a2bda77c19e5cba6dcd5b04da0638
bevzzz <dmytro.y.solovei@gmail.com>
2023-07-29 16:28:23 +0200

    feat: create basket and get recommended products

---
+++ ganymede-basket/src/pages/api/basket/create.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import connect from "@/middleware/database";
+
+interface CreateBasket {
+    user: 1;
+    products: string[];
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const mongo = await connect();
+    const baskets = mongo.db.collection("baskets")
+    const basket = await req.body.json() as CreateBasket;
+    const result = await baskets.insertOne({ ...basket, user: 1 })
+    res.status(201).json({ id: result.insertedId })
+}
+
+export default handler;
