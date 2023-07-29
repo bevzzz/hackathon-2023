70bb1a72d039e3800c946d1e15e7b12158c07fea
seyyid <seyyid.tiryaki@gmail.com>
2023-07-29 21:43:37 +0200

    feat: bottom nav links

--- ganymede-basket/src/components/footer/BottomNav.tsx
+++ ganymede-basket/src/components/footer/BottomNav.tsx
@@ -1,6 +1,7 @@
 import HomeIcon from '@mui/icons-material/Home';
 import AddIcon from '@mui/icons-material/Add';
 import ListIcon from '@mui/icons-material/List';
+import Link from 'next/link';
 
 type TPage = "home" | "add" | "list";
 
@@ -8,9 +9,9 @@ export default function BottomNav(props: { activePage: TPage }): JSX.Element {
     return (
         <div className='fixed w-full bottom-4 flex justify-center'>
             <div className='bg-[#04fbd3] p-3 rounded-xl flex gap-4 drop-shadow-lg'>
-                <HomeIcon htmlColor={props.activePage === "home" ? 'white' : 'grey'}></HomeIcon>
-                <AddIcon htmlColor={props.activePage === "add" ? 'white' : 'grey'}></AddIcon>
-                <ListIcon htmlColor={props.activePage === "list" ? 'white' : 'grey'}></ListIcon>
+                <Link href="/"><HomeIcon htmlColor={props.activePage === "home" ? 'white' : 'grey'}></HomeIcon></Link>
+                <Link href="/add"><AddIcon htmlColor={props.activePage === "add" ? 'white' : 'grey'}></AddIcon></Link>
+                <Link href="/list"><ListIcon htmlColor={props.activePage === "list" ? 'white' : 'grey'}></ListIcon></Link>
             </div>
         </div>
     )
