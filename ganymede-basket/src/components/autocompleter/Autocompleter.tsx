972998b2390f258b3717d2364f78826ed5718d29
seyyid <seyyid.tiryaki@gmail.com>
2023-07-29 20:28:50 +0200

    feat: add function

--- ganymede-basket/src/components/autocompleter/Autocompleter.tsx
+++ ganymede-basket/src/components/autocompleter/Autocompleter.tsx
@@ -1,9 +1,11 @@
-export default function Autocompleter(): JSX.Element {
+import { useState } from "react";
 
+export default function Autocompleter(props: { addItem: Function }): JSX.Element {
+    const [text, setText] = useState('');
     return (
-        <div className='mt-6 flex bg-slate-200 rounded-xl'>
-            <input type="text" placeholder='Name of Product...' className='autocomplete-input'></input>
-            <button className='m-auto pr-4 pl-2'>+</button>
+        <div className='mt-2 mb-4 flex bg-slate-200 rounded-xl'>
+            <input value={text} onKeyDown={(event: any) => { if (event.key === "Enter") props.addItem(text) }} onChange={e => setText(e.target.value)} type="text" placeholder='Name of Product...' className='autocomplete-input'></input>
+            <button onClick={() => props.addItem(text)} className='m-auto pr-4 pl-2'>+</button>
         </div>
     )
 }
